44e5d1237f133594cd6a889c7f07834f8f7e3b22
Richard Matthias <richardmatthias@gmail.com>
2016-07-28 00:08:10 +0100

    end video 48 (Blueprint rename)

--- Modules/System/utils.py
+++ Modules/System/utils.py
@@ -214,5 +214,13 @@ def addNodeToContainer(container, nodesIn, ihb=False, includeShapes=False, force
     cmds.container(container, edit=True, addNode=nodes, ihb=ihb, includeShapes=includeShapes, force=force)
 
 
+def doesBlueprintUserSpecifiedNameExist(name):
+    cmds.namespace(setNamespace=":")
+    namespaces = cmds.namespaceInfo(listOnlyNamespaces=True)
 
+    names = []
+    for namespace in namespaces:
+        if namespace.find("__") != -1:
+            names.append(namespace.partition("__")[2])
 
+    return name in names
